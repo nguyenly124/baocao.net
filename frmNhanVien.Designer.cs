913ab5b9ac271f3f78fe5370029c9bfa68d013fb
nguyenly124 <nguyenthily124da@gmail.com>
2024-11-29 18:12:14 +0700

    Add files via upload

---
+++ frmNhanVien.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace DONGHODEOTAY
+{
+    partial class frmNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNhanVien));
+            hienthi = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            groupBox1 = new GroupBox();
+            txtSodt = new TextBox();
+            txtDiachi = new TextBox();
+            txtTennv = new TextBox();
+            txtManv = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            groupBox2 = new GroupBox();
+            btxoa = new Button();
+            btsua = new Button();
+            bthem = new Button();
+            groupBox3 = new GroupBox();
+            bttimkiem = new Button();
+            txttimkiem = new TextBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)hienthi).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // hienthi
+            // 
+            hienthi.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            hienthi.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4 });
+            hienthi.Location = new Point(18, 82);
+            hienthi.Name = "hienthi";
+            hienthi.RowHeadersWidth = 30;
+            hienthi.Size = new Size(620, 427);
+            hienthi.TabIndex = 0;
+            hienthi.CellClick += hienthi_CellClick;
+            hienthi.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Mã nhân viên";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Tên nhân viên";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Địa chỉ ";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Số điện thoại";
+            Column4.MinimumWidth = 6;
+            Column4.Name = "Column4";
+            Column4.Width = 150;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(txtSodt);
+            groupBox1.Controls.Add(txtDiachi);
+            groupBox1.Controls.Add(txtTennv);
+            groupBox1.Controls.Add(txtManv);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(5, 8);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(357, 436);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông tin ";
+            // 
+            // txtSodt
+            // 
+            txtSodt.Location = new Point(14, 304);
+            txtSodt.Name = "txtSodt";
+            txtSodt.Size = new Size(337, 27);
+            txtSodt.TabIndex = 7;
+            // 
+            // txtDiachi
+            // 
+            txtDiachi.Location = new Point(14, 227);
+            txtDiachi.Name = "txtDiachi";
+            txtDiachi.Size = new Size(337, 27);
+            txtDiachi.TabIndex = 6;
+            // 
+            // txtTennv
+            // 
+            txtTennv.Location = new Point(14, 151);
+            txtTennv.Name = "txtTennv";
+            txtTennv.Size = new Size(328, 27);
+            txtTennv.TabIndex = 5;
+            // 
+            // txtManv
+            // 
+            txtManv.Location = new Point(14, 79);
+            txtManv.Name = "txtManv";
+            txtManv.Size = new Size(328, 27);
+            txtManv.TabIndex = 4;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(14, 270);
+            label5.Name = "label5";
+            label5.Size = new Size(101, 20);
+            label5.TabIndex = 3;
+            label5.Text = "Số điện thoại ";
+            label5.Click += label5_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(14, 195);
+            label4.Name = "label4";
+            label4.Size = new Size(59, 20);
+            label4.TabIndex = 2;
+            label4.Text = "Địa chỉ ";
+            label4.Click += label4_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(14, 120);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 20);
+            label3.TabIndex = 1;
+            label3.Text = "Tên nhân viên";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(14, 45);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Mã nhân viên";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btxoa);
+            groupBox2.Controls.Add(btsua);
+            groupBox2.Controls.Add(bthem);
+            groupBox2.Location = new Point(7, 454);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(355, 82);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Chức năng ";
+            // 
+            // btxoa
+            // 
+            btxoa.Location = new Point(246, 46);
+            btxoa.Name = "btxoa";
+            btxoa.Size = new Size(94, 29);
+            btxoa.TabIndex = 2;
+            btxoa.Text = "Xóa";
+            btxoa.UseVisualStyleBackColor = true;
+            btxoa.Click += btxoa_Click;
+            // 
+            // btsua
+            // 
+            btsua.Location = new Point(124, 46);
+            btsua.Name = "btsua";
+            btsua.Size = new Size(94, 29);
+            btsua.TabIndex = 1;
+            btsua.Text = "Sửa ";
+            btsua.UseVisualStyleBackColor = true;
+            btsua.Click += btsua_Click;
+            // 
+            // bthem
+            // 
+            bthem.Location = new Point(6, 46);
+            bthem.Name = "bthem";
+            bthem.Size = new Size(94, 29);
+            bthem.TabIndex = 0;
+            bthem.Text = "Thêm";
+            bthem.UseVisualStyleBackColor = true;
+            bthem.Click += bthem_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(bttimkiem);
+            groupBox3.Controls.Add(txttimkiem);
+            groupBox3.Controls.Add(label1);
+            groupBox3.Controls.Add(hienthi);
+            groupBox3.Location = new Point(378, 20);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(644, 516);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Dữ liệu ";
+            // 
+            // bttimkiem
+            // 
+            bttimkiem.BackgroundImage = (Image)resources.GetObject("bttimkiem.BackgroundImage");
+            bttimkiem.BackgroundImageLayout = ImageLayout.Stretch;
+            bttimkiem.Location = new Point(548, 30);
+            bttimkiem.Name = "bttimkiem";
+            bttimkiem.Size = new Size(35, 35);
+            bttimkiem.TabIndex = 3;
+            bttimkiem.UseVisualStyleBackColor = true;
+            bttimkiem.Click += bttimkiem_Click;
+            // 
+            // txttimkiem
+            // 
+            txttimkiem.Location = new Point(114, 34);
+            txttimkiem.Name = "txttimkiem";
+            txttimkiem.Size = new Size(416, 27);
+            txttimkiem.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Tìm kiếm ";
+            // 
+            // frmNhanVien
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSteelBlue;
+            ClientSize = new Size(1034, 548);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "frmNhanVien";
+            Text = "frmNhanVien";
+            Load += frmNhanVien_Load;
+            ((System.ComponentModel.ISupportInitialize)hienthi).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView hienthi;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private Label label1;
+        private TextBox txttimkiem;
+        private Button btxoa;
+        private Button btsua;
+        private Button bthem;
+        private Button bttimkiem;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox txtSodt;
+        private TextBox txtDiachi;
+        private TextBox txtTennv;
+        private TextBox txtManv;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+
+    }
+}
---
+++ frmNhanVien.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DONGHODEOTAY
+{
+    public partial class frmNhanVien : Form
+    {
+        public frmNhanVien()
+        {
+            InitializeComponent();
+        }
+        ketnoi kn = new ketnoi();
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void Getdata()
+        {
+
+
+            try
+            {
+                string query = "SELECT Manv, Tennv, Diachi, Sodt FROM NhanVien";
+                DataSet ds = kn.LayDuLieu(query);
+
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    hienthi.DataSource = ds.Tables[0]; // Gán dữ liệu vào DataGridView
+
+                }
+                else
+                {
+                    MessageBox.Show("Không có dữ liệu để hiển thị.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+
+        private void frmNhanVien_Load(object sender, EventArgs e)
+        {
+            hienthi.Columns["Column1"].DataPropertyName = "Manv";    // Mã NV
+            hienthi.Columns["Column2"].DataPropertyName = "Tennv";   // Tên NV
+            hienthi.Columns["Column3"].DataPropertyName = "Diachi";  // Địa chỉ
+            hienthi.Columns["Column4"].DataPropertyName = "Sodt";
+            Getdata();
+        }
+
+        private void hienthi_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                // Kiểm tra chỉ số hàng được click
+                if (e.RowIndex >= 0) // Đảm bảo không phải tiêu đề cột
+                {
+                    // Lấy hàng được chọn
+                    DataGridViewRow selectedRow = hienthi.Rows[e.RowIndex];
+
+                    // Gán giá trị vào các TextBox
+                    txtManv.Text = selectedRow.Cells["Column1"].Value?.ToString() ?? "";
+                    txtTennv.Text = selectedRow.Cells["Column2"].Value?.ToString() ?? "";
+                    txtDiachi.Text = selectedRow.Cells["Column3"].Value?.ToString() ?? "";
+                    txtSodt.Text = selectedRow.Cells["Column4"].Value?.ToString() ?? "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi chọn hàng: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void bthem_Click(object sender, EventArgs e)
+        {
+            if (kn.Connection.State != ConnectionState.Open)
+            {
+                kn.Connection.Open();
+            }
+
+            // Lấy thông tin từ các TextBox
+            string manv = txtManv.Text.Trim();
+            string tennv = txtTennv.Text.Trim();
+            string diachi = txtDiachi.Text.Trim();
+            string sodt = txtSodt.Text.Trim();
+
+            // Kiểm tra dữ liệu hợp lệ
+            if (string.IsNullOrEmpty(manv) || string.IsNullOrEmpty(tennv) || string.IsNullOrEmpty(diachi) || string.IsNullOrEmpty(sodt))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!long.TryParse(sodt, out _)) // Kiểm tra số điện thoại là số
+            {
+                MessageBox.Show("Số điện thoại phải là số!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Câu lệnh SQL thêm nhân viên
+            string query = "INSERT INTO NhanVien (Manv, Tennv, Diachi, Sodt) VALUES (@Manv, @Tennv, @Diachi, @Sodt)";
+
+            try
+            {
+
+                using (SqlCommand command = new SqlCommand(query, kn.Connection))
+                {
+                    // Gán giá trị cho các tham số
+                    command.Parameters.AddWithValue("@Manv", manv);
+                    command.Parameters.AddWithValue("@Tennv", tennv);
+                    command.Parameters.AddWithValue("@Diachi", diachi);
+                    command.Parameters.AddWithValue("@Sodt", sodt);
+
+                    // Thực hiện câu lệnh
+                    int result = command.ExecuteNonQuery();
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Thêm nhân viên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        ClearTextBoxes(); // Xóa dữ liệu sau khi thêm thành công
+                        Getdata(); // Làm mới danh sách nhân viên trên DataGridView
+                    }
+                    else
+                    {
+                        MessageBox.Show("Thêm nhân viên không thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void ClearTextBoxes()
+        {
+            txtManv.Clear();
+            txtTennv.Clear();
+            txtDiachi.Clear();
+            txtSodt.Clear();
+        }
+
+        private void btsua_Click(object sender, EventArgs e)
+        {
+            if (kn.Connection.State != ConnectionState.Open)
+            {
+                kn.Connection.Open();
+            }
+            // Lấy thông tin từ các TextBox
+            string manv = txtManv.Text.Trim();
+            string tennv = txtTennv.Text.Trim();
+            string diachi = txtDiachi.Text.Trim();
+            string sodt = txtSodt.Text.Trim();
+
+            // Kiểm tra dữ liệu hợp lệ
+            if (string.IsNullOrEmpty(manv) || string.IsNullOrEmpty(tennv) || string.IsNullOrEmpty(diachi) || string.IsNullOrEmpty(sodt))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!long.TryParse(sodt, out _)) // Kiểm tra số điện thoại là số
+            {
+                MessageBox.Show("Số điện thoại phải là số!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Câu lệnh SQL cập nhật
+            string query = "UPDATE NhanVien SET Tennv = @Tennv, Diachi = @Diachi, Sodt = @Sodt WHERE Manv = @Manv";
+
+            try
+            {
+                // Thực thi câu lệnh SQL
+                using (SqlCommand command = new SqlCommand(query, kn.Connection))
+                {
+                    // Gán giá trị cho các tham số
+                    command.Parameters.AddWithValue("@Manv", manv);
+                    command.Parameters.AddWithValue("@Tennv", tennv);
+                    command.Parameters.AddWithValue("@Diachi", diachi);
+                    command.Parameters.AddWithValue("@Sodt", sodt);
+
+                    // Thực hiện câu lệnh
+                    int result = command.ExecuteNonQuery();
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Sửa thông tin nhân viên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        ClearTextBoxes(); // Xóa dữ liệu trong các TextBox
+                        Getdata(); // Cập nhật lại danh sách trên DataGridView
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không tìm thấy nhân viên để sửa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btxoa_Click(object sender, EventArgs e)
+        {
+            if (kn.Connection.State != ConnectionState.Open)
+            {
+                kn.Connection.Open();
+            }
+            // Lấy thông tin từ các TextBox
+            string manv = txtManv.Text.Trim();
+            if (string.IsNullOrEmpty(manv))
+            {
+                MessageBox.Show("Vui lòng chọn nhân viên để xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            var tb = MessageBox.Show("Bạn có chắc chắn muốn xóa nhân viên này?", "Xác nhận xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (tb == DialogResult.Yes)
+            {
+                // SQL query to delete the employee by Manv
+                string query = "DELETE FROM NhanVien WHERE Manv = @Manv";
+                try
+                {
+                    // Thực thi câu lệnh SQL
+                    using (SqlCommand command = new SqlCommand(query, kn.Connection))
+                    {
+                        // Gán giá trị cho các tham số
+                        command.Parameters.AddWithValue("@Manv", manv);
+
+
+                        // Thực hiện câu lệnh
+                        int result = command.ExecuteNonQuery();
+
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Xóa thông tin nhân viên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            ClearTextBoxes(); // Xóa dữ liệu trong các TextBox
+                            Getdata(); // Cập nhật lại danh sách trên DataGridView
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không tìm thấy nhân viên để Xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Đã xảy ra lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+
+            }
+
+        }
+
+        private void bttimkiem_Click(object sender, EventArgs e)
+        {
+            
+                string searchText = txttimkiem.Text.Trim(); // Assuming txtSearch is the TextBox where user enters the search query
+
+                if (string.IsNullOrEmpty(searchText))
+                {
+                    MessageBox.Show("Vui lòng nhập thông tin tìm kiếm!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                try
+                {
+                    // Create the SQL query to search by employee ID or name
+                    string query = "SELECT Manv, Tennv, Diachi, Sodt FROM NhanVien WHERE Manv LIKE @SearchText OR Tennv LIKE @SearchText";
+                using (SqlCommand command = new SqlCommand(query, kn.Connection))
+                {
+                    command.Parameters.AddWithValue("@SearchText", "%" + searchText + "%"); // Thêm tham số @SearchText với giá trị đã được chuẩn bị
+
+                    // Tạo đối tượng DataAdapter để thực thi câu lệnh và lấy dữ liệu về
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataSet ds = new DataSet();
+
+                    // Lấy dữ liệu vào DataSet
+                    adapter.Fill(ds);
+
+                    if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                    {
+                        hienthi.DataSource = ds.Tables[0]; // Hiển thị kết quả tìm kiếm trong DataGridView
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không tìm thấy kết quả tìm kiếm.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Đã xảy ra lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        
+    }
+}
